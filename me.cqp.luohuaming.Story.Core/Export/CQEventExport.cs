8cd1fe5c2b94ecd5c57bb03efe5fc2685a84e512
Mr.喜 <863450594@qq.com>
2021-11-21 09:18:58 +0800

    更新版本号

--- me.cqp.luohuaming.Story.Core/Export/CQEventExport.cs
+++ me.cqp.luohuaming.Story.Core/Export/CQEventExport.cs
@@ -64,7 +64,7 @@ namespace me.cqp.luohuaming.Story.App.Export
 			// 反射获取 AppData 实例	
 			Type appDataType = typeof (AppData);	
 			// 注册一个 CQApi 实例	
-			AppInfo appInfo = new AppInfo ("me.cqp.luohuaming.Story", 1, 9, "水银续写机", "1.0.0", 1, "落花茗", "基于彩云小梦尝鲜版的续写Bot", authCode);	
+			AppInfo appInfo = new AppInfo ("me.cqp.luohuaming.Story", 1, 9, "水银续写机", "1.0.1", 1, "落花茗", "基于彩云小梦尝鲜版的续写Bot", authCode);	
 			appDataType.GetRuntimeProperty ("CQApi").GetSetMethod (true).Invoke (null, new object[] { new CQApi (appInfo) });	
 			AppData.UnityContainer.RegisterInstance<CQApi> ("me.cqp.luohuaming.Story", AppData.CQApi);	
 			// 向容器注册一个 CQLog 实例	
