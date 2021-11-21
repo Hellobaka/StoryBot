9f56d59791daa4f26431668466ca578cbe394e6c
Mr.喜 <863450594@qq.com>
2021-11-21 09:18:11 +0800

    帮助文本修正

--- me.cqp.luohuaming.Story.Code/OrderFunctions/Help.cs
+++ me.cqp.luohuaming.Story.Code/OrderFunctions/Help.cs
@@ -18,7 +18,7 @@ namespace me.cqp.luohuaming.Story.Code.OrderFunctions
 
         public bool Judge(string destStr) => destStr.StartsWith(GetOrderStr());
 
-        string help = "公用接口，请注意言论，避免高频率调用。在进行续写时请写有意义的句子，避免浪费彩云小梦官方的计算资源浪费。\n" +
+        string help = "公用接口，请注意言论，避免高频率调用。在进行续写时请写有意义的句子，避免浪费彩云小梦官方的计算资源。\n" +
     "#创建续写 新建一篇续写，一个来源只能同时拥有一篇续写，10分钟无操作自动销毁\n" +
     "#结束续写 结束续写，可重新开始一次\n" +
     "续写 [内容(首次必需)] 只有当前来源存在一篇续写时可以调用。第一次使用请加上一个开头，之后再调用可直接写‘续写’两个字";
